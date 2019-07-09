fbf5c83f6e925d235bf69b05c8cd6860051518a6
C1310214 Erik Da Silva Paixao Reis <>
2019-07-09 13:35:28 -0300

    Corrigido erros nos teste

--- src/test/java/paixao/erik/resources/UserResourceTest.java
+++ src/test/java/paixao/erik/resources/UserResourceTest.java
@@ -1,14 +1,47 @@
 package paixao.erik.resources;
 
-import paixao.erik.entitys.user.User;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import paixao.erik.entitys.user.User;
 import paixao.erik.resources.user.UserResource;
 
 @QuarkusTest
-class UserResourceTest extends GenericResourceTest<User, UserResource> {
+public class UserResourceTest {
 
-    UserResourceTest() {
-        super(UserResource.class, new User("5d23d0aa79c88e4e4e2747ab", "erik@erik.com", "123456"));
+    public UserResource userResource;
+    public String uuid;
+    public User user;
+
+    @BeforeEach
+    public void setUp() {
+        userResource = Mockito.mock(UserResource.class);
+        uuid = "5d23d0aa79c88e4e4e2747ab";
+        user = new User(uuid, "erik@erik.com", "123456");
     }
 
+    @Test
+    public void get() {
+        userResource.get(uuid);
+        Mockito.verify(userResource).get(uuid);
+    }
+
+    @Test
+    public void save() {
+        userResource.save(user);
+        Mockito.verify(userResource).save(user);
+    }
+
+    @Test
+    public void delete() {
+        userResource.delete(uuid);
+        Mockito.verify(userResource).delete(uuid);
+    }
+
+    @Test
+    public void getAll() {
+        userResource.getAll();
+        Mockito.verify(userResource).getAll();
+    }
 }
--- src/test/java/paixao/erik/services/UserServiceTest.java
+++ src/test/java/paixao/erik/services/UserServiceTest.java
@@ -1,14 +1,47 @@
 package paixao.erik.services;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import paixao.erik.entitys.user.User;
 import paixao.erik.services.user.UserService;
-import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
-class UserServiceTest extends GenericServiceTest<User, UserService> {
+public class UserServiceTest {
 
-    UserServiceTest() {
-        super(UserService.class, new User("5d23d0aa79c88e4e4e2747ab", "erik@erik.com", "123456"));
+    public UserService userService;
+    public String uuid;
+    public User user;
+
+    @BeforeEach
+    public void setUp() {
+        userService = Mockito.mock(UserService.class);
+        uuid = "5d23d0aa79c88e4e4e2747ab";
+        user = new User(uuid, "erik@erik.com", "123456");
     }
 
+    @Test
+    public void getAll() {
+        userService.getAll();
+        Mockito.verify(userService).getAll();
+    }
+
+    @Test
+    public void get() {
+        userService.getOne(uuid);
+        Mockito.verify(userService).getOne(uuid);
+    }
+
+    @Test
+    public void save() {
+        userService.save(user);
+        Mockito.verify(userService).save(user);
+    }
+
+    @Test
+    public void delete() {
+        userService.delete(uuid);
+        Mockito.verify(userService).delete(uuid);
+    }
 }
